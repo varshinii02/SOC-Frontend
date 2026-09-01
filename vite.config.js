90b81d740680e1dcc31f8ae8f33572dea512bdc4
Varshini <varshinii1702@gmail.com>
2026-09-01 16:21:25 +0530

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SOC-Frontend/',
   server: {
     watch: {
       ignored: ['**/public/fight.yaml'],
